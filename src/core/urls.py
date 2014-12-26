b33b334cef827d7643242f21e6d87014cf899c2b
Marco Rougeth <marco@rougeth.com>
2014-12-26 12:15:59 -0200

    Refactored login view and start using django.contrib.auth

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('core.views',
+
+urlpatterns = patterns(
+    'core.views',
     url(r'^$', 'home', name='home'),
-    url(r'^login$', 'login', name='login'),
 )
--- src/regcert/urls.py
+++ src/regcert/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'regcert.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login$', 'django.contrib.auth.views.login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout'),
     url(r'^', include('core.urls')),
 )
