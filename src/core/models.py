eef3d5062a9010f19ce94d6f913ce7c96c5ccc85
Marco Rougeth <marco@rougeth.com>
2015-01-05 16:00:16 -0200

    Initializated student, course, enrolment and certificate models

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=40)
+    birthday = models.DateField()
+    nationality = models.CharField(max_length=40)
+    rg = models.CharField(max_length=15)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=40)
+    hours = models.IntegerField()
+    begin = models.DateField()
+    end = models.DateField()
+
+
+class Enrolment(models.Model):
+    student = models.ForeignKey(Student)
+    course = models.ForeignKey(Course)
+
+
+class Certificate(models.Model):
+    enrolment = models.ForeignKey(Enrolment)
+    date_time = models.DateTimeField()
