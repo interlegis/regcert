53f995f173dbd1f247c3c5b2c02fa77107f3a716
Marco Rougeth <marco@rougeth.com>
2015-01-08 15:45:33 -0200

    Added student's delete view

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from core.views import StudentCreate, StudentView, StudentUpdate
+from core.views import StudentCreate, StudentView, StudentUpdate, StudentDelete
 
 
 urlpatterns = patterns(
@@ -8,5 +8,7 @@ urlpatterns = patterns(
     url(r'^alunos/cadastrar$', StudentCreate.as_view(), name='student_create'),
     url(r'^alunos/editar/(?P<pk>[\w-]+)$', StudentUpdate.as_view(),
         name='student_update'),
+    url(r'^alunos/remover/(?P<pk>[\w-]+)$', StudentDelete.as_view(),
+        name='student_delete'),
     url(r'^alunos$', StudentView.as_view(), name='students'),
 )
--- src/core/views.py
+++ src/core/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 
 from core.models import Student
@@ -29,6 +29,13 @@ class StudentCreate(LoginRequiredMixin, CreateView):
     model = Student
     success_url = '/alunos'
 
+
 class StudentUpdate(LoginRequiredMixin, UpdateView):
     model = Student
+    success_url = '/alunos'
+
+
+class StudentDelete(LoginRequiredMixin, DeleteView):
+    model = Student
+    context_object_name = 'student'
     success_url = '/alunos'
