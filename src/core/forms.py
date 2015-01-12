3c7611dcc75bfd1a3474e5a18bd812dce964fc01
Marco Rougeth <marco@rougeth.com>
2015-01-12 14:40:10 -0200

    Refactored Student's create view to add enrollment when saving a new student

---
+++ src/core/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from core.models import Course, Student
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fiels = ['name', 'birthday', 'nationality', 'rg']
+
+
+class StudentCoursesForm(forms.Form):
+    courses = forms.ModelMultipleChoiceField(queryset=Course.objects.all(),
+        widget=forms.CheckboxSelectMultiple)
--- src/core/views.py
+++ src/core/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 
-from core.models import Certificate, Course, Student
+from core.forms import StudentForm, StudentCoursesForm
+from core.models import Certificate, Course, Enrollment, Student
 
 
 class LoginRequiredMixin(object):
@@ -25,9 +26,41 @@ class StudentView(LoginRequiredMixin, ListView):
     context_object_name = 'students'
 
 
-class StudentCreate(LoginRequiredMixin, CreateView):
-    model = Student
-    success_url = '/alunos'
+class StudentCreate(View):
+
+    def get(self, request):
+        student_form = StudentForm(prefix='student')
+        student_courses_form = StudentCoursesForm(prefix='student_courses')
+
+        context = {
+            'student_form': student_form,
+            'student_courses_form': student_courses_form
+        }
+        return render(request, 'core/student_create.html', context)
+
+    def post(self, request):
+        student_form = StudentForm(request.POST, prefix='student')
+        student_courses_form = StudentCoursesForm(request.POST,
+                                                  prefix='student_courses')
+
+        if student_form.is_valid() and student_courses_form.is_valid():
+            student = student_form.save()
+            data = student_courses_form.clean()
+            print(data)
+            for course in data['courses']:
+
+                c = Enrollment(
+                    student=student,
+                    course=course
+                )
+                c.save()
+            return HttpResponseRedirect('/alunos')
+        else:
+            context = {
+                'student_form': student_form,
+                'student_courses_form': student_courses_form
+            }
+        return render(request, 'core/student_create.html', context)
 
 
 class StudentUpdate(LoginRequiredMixin, UpdateView):
