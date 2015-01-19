35a3b12b410e056750901559f0227539b465fee3
Marco Rougeth <marco@rougeth.com>
2015-01-19 17:02:07 -0200

    Defined i18n settings

--- src/regcert/settings.py
+++ src/regcert/settings.py
@@ -73,7 +73,11 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+LANGUAGE_CODE = 'pt_BR'
 
 TIME_ZONE = 'UTC'
 
