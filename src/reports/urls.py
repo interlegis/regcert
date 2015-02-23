358f1c60bd70a7bbe27069d8c93ef5b550953665
Marco Rougeth <marco@rougeth.com>
2015-02-23 13:59:31 -0300

    Added new report with valid and invalid certificates

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
-from reports.views import ReportAllCertificates
+from reports.views import ReportAllValidCertificates, ReportAllCertificates
 
 
 urlpatterns = patterns(
     'reports.views',
-    url(r'^certificados$', ReportAllCertificates.as_view(),
+    url(r'^certificados_validos$', ReportAllValidCertificates.as_view(),
+        name='report_all_certificates'),
+    url(r'^todos_certificados$', ReportAllCertificates.as_view(),
         name='report_all_certificates'),
 )
--- src/reports/views.py
+++ src/reports/views.py
@@ -1,6 +1,22 @@
+from itertools import chain
+
 from easy_pdf.views import PDFTemplateView
 
-from certificate.models import Certificate
+from certificate.models import Certificate, InvalidCertificate
+
+
+class ReportAllValidCertificates(PDFTemplateView):
+    template_name = 'reports/all_certificates.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ReportAllValidCertificates, self).get_context_data(
+            pagesize="A4",
+            title="Hi there!",
+            **kwargs)
+        context['certificates'] = Certificate.objects.all()
+        context['title'] = 'Relatório Regcert: todos os certificados \
+                            cadastrados'
+        return context
 
 
 class ReportAllCertificates(PDFTemplateView):
@@ -11,5 +27,14 @@ class ReportAllCertificates(PDFTemplateView):
             pagesize="A4",
             title="Hi there!",
             **kwargs)
-        context['certificates'] = Certificate.objects.all()
+        valid_certificates = Certificate.objects.order_by('book_number')
+        invalid_certificates = InvalidCertificate.objects.order_by(
+            'book_number')
+        all_certificates = chain(valid_certificates, invalid_certificates)
+        context['has_invalid_certificates'] = True
+        context['certificates'] = sorted(all_certificates,
+                                         key=lambda c: c.book_number)
+        context['title'] = 'Relatório Regcert: todos os certificados \
+                            cadastrados e anulados'
+
         return context
