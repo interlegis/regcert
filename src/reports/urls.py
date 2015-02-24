f51448fa7aebd69893d60dde06ada9a38ee2d74a
Marco Rougeth <marco@rougeth.com>
2015-02-24 10:42:20 -0300

    Added certificate report by course name

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, url
 
-from reports.views import ReportAllValidCertificates, ReportAllCertificates
+from reports.views import (ReportsView, ReportAllValidCertificates,
+                           ReportAllCertificates, ReportCertificatesByCourse)
 
 
 urlpatterns = patterns(
     'reports.views',
-    url(r'^certificados_validos$', ReportAllValidCertificates.as_view(),
+    url(r'^$', ReportsView.as_view(),
+        name='reports'),
+    url(r'^certificados/todos$', ReportAllCertificates.as_view(),
         name='report_all_certificates'),
-    url(r'^todos_certificados$', ReportAllCertificates.as_view(),
-        name='report_all_certificates'),
+    url(r'^certificados/validos$', ReportAllValidCertificates.as_view(),
+        name='report_all_valid_certificates'),
+    url(r'^certificados/curso/(?P<course_name>[\w-]+)$',
+        ReportCertificatesByCourse.as_view(),
+        name='report_certificates_by_course'),
 )
--- src/reports/views.py
+++ src/reports/views.py
@@ -37,5 +37,27 @@ class ReportAllCertificates(PDFTemplateView):
                                          key=lambda c: c.book_number)
         context['title'] = u'Relatório Regcert: todos os certificados \
                             cadastrados e anulados'
+        return context
 
+
+class ReportCertificatesByCourse(PDFTemplateView):
+    template_name = 'reports/all_certificates.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if not context['certificates']:
+            return HttpResponseNotFound('<h1>Page not found</h1>')
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super(ReportCertificatesByCourse, self).get_context_data(
+            pagesize="A4",
+            title="Hi there!",
+            **kwargs)
+        course_name = kwargs['course_name'].replace('-', ' ')
+        context['certificates'] = Certificate.objects.filter(
+            course_name=course_name)
+        context['title'] = u'Relatório Regcert: todos os certificados do \
+                             curso de pós-graduação lato sensu em {} \
+                             do ILB'.format(course_name)
         return context
